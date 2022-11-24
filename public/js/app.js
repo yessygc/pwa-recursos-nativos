7f652b28bedf51ba631cab09ca0e89a65f4f7723
Yessyca Garcia Campos <yessgarciacampos@gmail.com>
2022-11-23 22:37:42 -0600

    Share API

--- public/js/app.js
+++ public/js/app.js
@@ -89,6 +89,8 @@ function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
 
     var content =`
     <li class="animated fadeIn fast"
+        data-user="${ personaje }"
+        data-mensaje="${ mensaje }"
         data-tipo="mensaje">
 
 
@@ -530,5 +532,36 @@ btnTomarFoto.on('click', () => {
 
 // Share API
 
+// if ( navigator.share ) {
+//     console.log('Navigador lo soporta');
+// } else {
+//     console.log('Navigador NO lo soporta');
+// }
 
+timeline.on('click', 'li', function() {
+    // console.log( $(this) );
+    // console.log( $(this).data('tipo') );
+    // console.log( $(this).data('user') );
 
+    let tipo    = $(this).data('tipo');
+    let lat     = $(this).data('lat');
+    let lng     = $(this).data('lng');
+    let mensaje = $(this).data('mensaje');
+    let user    = $(this).data('user');
+
+    console.log({ tipo, lat, lng, mensaje, user });
+
+    const shareOpts = {
+        title: user,
+        text: mensaje
+    };
+
+    if (tipo === 'mapa') {
+        shareOpts.text = 'Mapa';
+        shareOpts.url  = `https://www.google.com/maps/@${ lat },${ lng },15z`;
+    }
+
+    navigator.share(shareOpts)
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+});
